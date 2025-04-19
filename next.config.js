52f0d388fe57066f1be74d681cc807de75386831
vamsi krishna nagidi <vamsikrishna.nagidi@gmail.com>
2025-04-19 13:59:17 +0530

    Media fixed

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['api.hirelink.com', 'hirelink-api.onrender.com'],
+    domains: ['api.hirelink.com', 'hirelink-api.onrender.com', 'images.unsplash.com'],
   },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
