b603e38d4a61815863d16bbdac719cd7eeb82e5a
vamsi krishna nagidi <vamsikrishna.nagidi@gmail.com>
2025-04-26 23:24:08 +0530

    Top navbar hide show

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -2,12 +2,36 @@
 
 import { FiMenu } from 'react-icons/fi';
 import styles from '../app/dashboard/candidate/page.module.css';
+import { useState, useEffect } from 'react';
 
 export default function TopNavbar({ isMobile, showSidebar, handleMenuClick }) {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const controlNavbar = () => {
+      const currentScrollY = window.scrollY;
+      
+      if (currentScrollY > lastScrollY) { // scrolling down
+        setIsVisible(false);
+      } else { // scrolling up
+        setIsVisible(true);
+      }
+      
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener('scroll', controlNavbar);
+
+    return () => {
+      window.removeEventListener('scroll', controlNavbar);
+    };
+  }, [lastScrollY]);
+
   return (
     <>
     {isMobile &&(
-    <div className={styles.topNavbar}>
+    <div className={`${styles.topNavbar} ${!isVisible ? styles.hidden : ''}`}>
       <div className={styles.navbarBrand}>
         <h4 style={{ color: '#000000' }}>Zyukthi</h4>
       </div>
